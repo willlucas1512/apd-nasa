9981da323b1d7fd85a972a61d9ecbabda068ad4e
willlucas1512 <41302062+willlucas1512@users.noreply.github.com>
2022-06-23 22:08:34 -0300

    feat:Adiciona pesquisa por nome e data

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,32 +1,85 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "./List.module.css";
 import earth from "./earth.gif";
 import { formatDate } from "../../utils/date";
 
 function List() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchResult, setSearchResult] = useState([]);
+
+  const dataRef = useRef([]);
+  const requestSize = useRef(10);
+
+  const handleNameSearch = (pEvent) => {
+    const xValue = pEvent.target.value.replace(/\s/g, "").toLowerCase();
+    if (Object.keys(xValue).length === 0) {
+      setSearchResult([]);
+      return;
+    }
+    const xDataToCompare = data.map(({ title }) =>
+      title.replace(/\s/g, "").toLowerCase()
+    );
+    const xResult = xDataToCompare.filter((pElement) =>
+      pElement.includes(xValue)
+    );
+
+    let xSearchResult = [];
+
+    if (xResult.length > 0) {
+      xResult.forEach((pItem) =>
+        xSearchResult.push(data[xDataToCompare.indexOf(pItem)])
+      );
+    }
+
+    setSearchResult(xSearchResult);
+  };
+
+  const handleDateSearch = (pEvent) => {
+    const xValue = pEvent.target.value;
+    getData({ date: xValue });
+  };
+
+  const getData = async (pParams) => {
+    try {
+      const xHasDate = pParams?.date;
+      const xParams = xHasDate
+        ? `&date=${pParams?.date}`
+        : `&count=${requestSize.current}`;
+      const rResponse = await fetch(process.env.REACT_APP_API_URL + xParams);
+      if (!rResponse.ok) {
+        throw new Error(
+          `This is an HTTP error: The status is ${rResponse.status}`
+        );
+      }
+      let xJsonData = await rResponse.json();
+      const xData = xHasDate ? [xJsonData] : xJsonData;
+      setData(xData);
+      setError(null);
+    } catch (rErr) {
+      setError(rErr.message);
+      setData([]);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await fetch(process.env.REACT_APP_API_URL);
-        if (!response.ok) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        let actualData = await response.json();
-        setData(actualData);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setData({});
-      } finally {
-        setLoading(false);
-      }
-    };
+    if (searchResult.length > 0) {
+      setData(searchResult);
+    } else {
+      setData(dataRef.current);
+    }
+  }, [searchResult]);
+
+  useEffect(() => {
+    if (data.length === requestSize.current) {
+      dataRef.current = data;
+    }
+  }, [data]);
+
+  useEffect(() => {
     getData();
   }, []);
 
@@ -63,19 +116,26 @@ function List() {
         <div className={styles.container}>
           <div className={styles.inputs}>
             <div className={styles.searchBox}>
+              <div className={styles.icon}>
+                <span className={"material-icons md-18"}>search</span>
+              </div>
               <input
+                name="nome"
+                onChange={handleNameSearch}
                 className={styles.searchInput}
                 type="text"
                 placeholder="Procure por nome ou selecione uma data"
               />
             </div>
-            <input className={styles.dateSelector} type="date" />
-            <button className={styles.searchBtn}>
-              <span className="material-icons md-18">search</span>
-            </button>
+            <input
+              onChange={handleDateSearch}
+              name="data"
+              className={styles.dateSelector}
+              type="date"
+            />
           </div>
           <div className={styles.list}>
-            {Object.values(data).map((pItem, pIndex) => {
+            {data.map((pItem, pIndex) => {
               return (
                 <div key={pIndex} className={styles.item}>
                   <img src={pItem.url} />
