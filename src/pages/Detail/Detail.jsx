4cd4ec2eb145d532e9c1aca92bd85a1f07c65159
willlucas1512 <41302062+willlucas1512@users.noreply.github.com>
2022-06-25 04:25:21 -0300

    feat:Adiciona tratamento de erro

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,35 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./Detail.module.css";
 import { formatDate } from "../../utils/date";
+import ErrorFeedback from "../../components/ErrorFeedback";
+import styles from "./Detail.module.css";
+import PropTypes from "prop-types";
 
 function Detail(props) {
-  const { url, title, date, explanation } = props.location.state.item;
-
-  return (
-    <>
-      <div className={styles.titleDiv}>
-        <Link style={{ zIndex: 9999 }} to="/list">
-          <div className={styles.icon}>
-            <span className={"material-icons"}>arrow_back</span>
-          </div>
-        </Link>
-        <h2 className={styles.title}>Astronomy Picture of the Day</h2>
-      </div>
-      <div className={styles.container}>
-        <div className={styles.item}>
-          <div className={styles.imgContainer}>
-            <img src={url} />
-          </div>
-          <div className={styles.text}>
-            <b>{title}</b>
-          </div>
-          <div className={styles.text}>{formatDate(date)}</div>
-          <div className={styles.textExplanation}>{explanation}</div>
+  if (props.location?.state) {
+    const { url, title, date, explanation } = props.location.state.item;
+    const xFormattedDate = formatDate(date);
+    return (
+      <>
+        <div className={styles.titleDiv}>
+          <Link to="/list">
+            <div className={styles.icon}>
+              <span className={"material-icons"}>arrow_back</span>
+            </div>
+          </Link>
+          <h2 className={styles.title}>Astronomy Picture of the Day</h2>
         </div>
-      </div>
-    </>
-  );
+        <div className={styles.container}>
+          <div className={styles.item}>
+            <div className={styles.imgContainer}>
+              <a href={url}>
+                <img
+                  alt={`Nasa's astronomy of the day from ${xFormattedDate}`}
+                  className={styles.img}
+                  src={url}
+                />
+              </a>
+              <span className={styles.label}>Click to open image</span>
+            </div>
+            <div className={styles.texts}>
+              <div className={styles.text}>
+                <b>{title}</b>
+              </div>
+              <div className={styles.date}>{xFormattedDate}</div>
+              <div className={styles.textExplanation}>{explanation}</div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <ErrorFeedback
+        message={
+          "It is not possible to access this route directly. Please navigate through interface."
+        }
+      />
+    );
+  }
 }
 
 export default Detail;
+Detail.propTypes = {
+  location: PropTypes.object,
+};
